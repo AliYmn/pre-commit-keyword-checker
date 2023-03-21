0c4cdc53f84677792f7671d5399f08ed30476625
aliyaman <aliymnmonotech@gmail.com>
2023-03-22 00:50:43 +0300

    version upgraded

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="pre-commit-keyword-checker",
-    version="0.1.0",
-    description="Pre-commit hook to detect specified keywords in files being committed",
+    version="0.2.1",
+    description="A pre-commit hook for checking specified keywords in the files being committed.",
     author="Ali Yaman",
     author_email="aliymn.db@gmail.com",
+    url="https://github.com/AliYmn/pre-commit-keyword-checker",
     packages=find_packages(),
     install_requires=["pre-commit"],
     entry_points={
