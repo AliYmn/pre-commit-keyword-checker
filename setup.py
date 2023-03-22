b3f72ac9c7f734a423dcf83cabf117c59a5f7918
aliyaman <aliymnmonotech@gmail.com>
2023-03-22 12:21:34 +0300

    IN-1071: hotfix

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
+import sys
+
+if sys.version_info[0] < 3:
+    with open('README.md') as f:
+        README = f.read()
+else:
+    with open('README.md', encoding='utf-8') as f:
+        README = f.read()
 
 setup(
     name="pre-commit-keyword-checker",
-    version="0.2.1",
+    version="1.1.0",
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=README,
     description="A pre-commit hook for checking specified keywords in the files being committed.",
     author="Ali Yaman",
     author_email="aliymn.db@gmail.com",
