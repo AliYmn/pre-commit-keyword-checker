c2654787477743ed8fed4a889fa3f88e8fbc5da2
aliyaman <aliymnmonotech@gmail.com>
2023-03-22 12:41:44 +0300

    version upgraded

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name="pre-commit-keyword-checker",
-    version="1.4.0",
+    version="1.5.0",
     license="MIT",
     long_description_content_type="text/markdown",
     long_description=README,
